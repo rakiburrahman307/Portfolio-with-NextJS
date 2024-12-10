de5611ccad4a111326c70491bf2d9d5b4f408cd6
rakiburrahman307 <rakiburrahman307@gmail.com>
2024-12-10 23:26:14 +0600

    optimise images

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      unoptimized: true, // Disable image optimization
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
